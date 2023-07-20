a7a9ae5c574a9b2812a6008bb783fd1c1c4cf7aa
Ali <a_parhizkar2002@yahoo.com>
2023-07-20 17:43:07 +0330

    Delete Help document from project

--- Main/Data/Code/rjrekbsr.a0i.cs
+++ Main/Data/Code/rjrekbsr.a0i.cs
@@ -9,19 +9,18 @@ namespace Caspian.Dynamic.WorkflowForm
 	{
 		public void Initialize()
 		{
-			cmbEmploymentOrderType.TextExpression = t => t.Code + "- " + t.Title;
+			cmbEmploymentOrderType.TextExpression = t => t.Code + " " +  t.Title;
 		}
 
 		public async Task EmploymentOrderType_OnChange()
 		{
-			var id = employmentOrder.EmploymentOrderTypeId;
-			if (id > 0)
-			{
+            var id = employmentOrder.EmploymentOrderTypeId;
+            if (id > 0 && (employmentOrder.Descript == null || await Confirm("Do you change the value of ")))
+            {
                 using var service = CreateScope().GetService<EmploymentOrderTypeService>();
                 var old = await service.SingleAsync(id);
-                if (!employmentOrder.Descript.HasValue() || await Confirm("آیا با تغییر شرح حکم موافقید؟"))
-                    employmentOrder.Descript = old.Description;
-			}
+                employmentOrder.Descript = old.Description;
+            }
 		}
 
 	 	public void OrganUnit_OnChange()
@@ -37,6 +36,11 @@ namespace Caspian.Dynamic.WorkflowForm
 		public void Major_OnChange()
 		{
 			
+		}
+
+		public void Checking_OnChange()
+		{
+			Alert("You selected " + Checking.ToString());
 		}
     }
 }
