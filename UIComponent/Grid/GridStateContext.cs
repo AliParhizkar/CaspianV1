b4d85c39c401034bb056cb42c9f99443fd65e6a9
naser.parhizkar.se@gmail.com <naser.parhizkar.se@gmail.com>
2024-01-14 11:44:40 +0330

    Updating projects .Net versions and package references and also fixing a bug related to grid persistence state

---
+++ UIComponent/Grid/GridStateContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIComponent.Grid
+{
+    public class GridStateContext
+    {
+        public Func<string,string, Task> SaveGridStateAsync;
+        public Func<string, Task<string>> GetGridStateAsync;
+    }
+}
