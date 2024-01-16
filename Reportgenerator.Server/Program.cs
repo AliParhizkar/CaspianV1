112460fd91210fffec6f908011b64f6ef96084f1
AliParhizkar <a_parhizkar2002@yahoo.com>
2024-01-17 01:29:18 +0330

    Add client Project to server

--- Reportgenerator.Server/Program.cs
+++ Reportgenerator.Server/Program.cs
@@ -8,30 +8,25 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        CS.Con = builder.Configuration.GetConnectionString("CaspianDb");
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-        builder.Services.AddScoped<CaspianDataService>();
-        builder.Services.AddScoped<Context>();
-        builder.Services.AddScoped<ReportService, ReportService>();
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddRazorPages();
+
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+            app.UseWebAssemblyDebugging();
+        else
+            app.UseHsts();
 
         app.UseHttpsRedirection();
+        app.UseBlazorFrameworkFiles();
+        app.UseStaticFiles();
 
-        app.UseAuthorization();
+        app.UseRouting();
 
-
+        app.MapRazorPages();
         app.MapControllers();
+        app.MapFallbackToFile("index.html");
 
         app.Run();
     }
