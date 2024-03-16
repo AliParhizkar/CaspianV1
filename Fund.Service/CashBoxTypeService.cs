558040086c4f43e3c47c41bf2584f3c3ef119f5e
naser.parhizkar.se@gmail.com <naser.parhizkar.se@gmail.com>
2024-03-16 12:36:14 +0330

    currency added

---
+++ Fund.Service/CashBoxTypeService.cs
@@ -0,0 +1,19 @@
+﻿using Caspian.Common;
+using Caspian.Common.Service;
+using FluentValidation;
+using Fund.Model;
+
+namespace Fund.Service
+{
+    public class CashBoxTypeService : BaseService<CashBoxType>, IBaseService<CashBoxType>
+    {
+        public CashBoxTypeService(IServiceProvider provider)
+            : base(provider)
+        {
+            RuleFor(t => t.Code).Required();
+            RuleFor(t => t.Name).MaximumLength(50).Required();
+            RuleFor(t => t.InternationalName).MaximumLength(50);
+            RuleFor(t => t.InternationalName).MaximumLength(200);
+        }
+    }
+}
---
+++ Fund.Service/CurrencyService.cs
@@ -0,0 +1,17 @@
+﻿using Caspian.Common;
+using Caspian.Common.Service;
+using FluentValidation;
+using Fund.Model;
+
+namespace Fund.Service
+{
+    public class CurrencyService : BaseService<Currency>, IBaseService<Currency>
+    {
+        public CurrencyService(IServiceProvider provider)
+            : base(provider)
+        {
+            RuleFor(t => t.Code).MaximumLength(50).Required();
+            RuleFor(t => t.Name).MaximumLength(50).Required();
+        }
+    }
+}
